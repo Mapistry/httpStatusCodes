8007108cefe721f399d1cfddd2b793ea39e6151b
Austin Brown <thataustin@gmail.com>
2014-10-13 11:04:58 -0700

    Updating wording

--- index.js
+++ index.js
@@ -13,29 +13,29 @@ codes.SUCCESS = 200;
 codes.INVALID_REQUEST = 400;
 
 /**
- * Returned when a request is made by someone we don't recognize at all
+ * Returned when a request is made by someone we don't recognize at all.
  * (ie, invalid or no credentials offered alongside the request)
  */
 codes.INVALID_CREDENTIALS = 401;
 
 /**
- * Returned when a known user is trying to access something that doesn't belong to them
+ * Returned when a known user is trying to access something that doesn't belong to them.
  */
 codes.UNAUTHORIZED_REQUEST = 403;
 
 /**
- * Used for missing query params in the URL
+ * Used for missing query params in the URL.  This is an application-level error.
  */
 codes.MISSING_QUERY_PARAM = 422;
 
 /**
- * Used most often for missing data in a POST, PUT or PATCH request
+ * Used most often for missing data in a POST, PUT or PATCH request.  This is an application-level error.
  */
 codes.MISSING_DATA = 422;
 
 /**
  * This is an unexpected error that should be resolved soon.  A backoff
- * retry approach is usually a good way to handle 500's
+ * retry approach is usually a good way to handle 500's.
  */
 codes.SERVER_ERROR = 500;
 
